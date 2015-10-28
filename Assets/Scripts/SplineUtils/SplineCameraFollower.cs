289947d8a8c7697ed13509dde51e33d5accdc468
Flafla2 <awesomesauceface@aol.com>
2015-10-27 23:34:42 -0400

    Finished Camera Follower

--- Assets/Scripts/SplineUtils/SplineCameraFollower.cs
+++ Assets/Scripts/SplineUtils/SplineCameraFollower.cs
@@ -10,6 +10,8 @@ public class SplineCameraFollower : MonoBehaviour {
 
 	private int CurrentShot = 0;
 	private float SplineProgress = 0;
+    private float FadeStartTime = -1;
+    private FadeState CurrentFade = FadeState.None;
 	private Material FadeMaterial;
 
 	void Start () {
@@ -21,8 +23,53 @@ public class SplineCameraFollower : MonoBehaviour {
 	}
 
 	void Update () {
-		
-	}
+        ApplyShot();
+        HandleShotState();
+
+        if (CurrentFade != FadeState.None)
+        {
+            Color col = FadeMaterial.GetColor("_Color");
+            float alpha = Mathf.Clamp01((Time.time - FadeStartTime) / FadeLength);
+            
+            if (CurrentFade == FadeState.FadeIn)
+                alpha = 1 - alpha;
+
+            col.a = alpha;
+            FadeMaterial.SetColor("_Color", col);
+        }
+    }
+
+    private void HandleShotState()
+    {
+        if (CurrentFade == FadeState.None)
+        {
+            SplineProgress += (Shots[CurrentShot].Velocity / Shots[CurrentShot].Path.GetDeriv(SplineProgress).magnitude)*Time.deltaTime;
+            Vector3 diff = Shots[CurrentShot].Path.GetPoint(Shots[CurrentShot].Path.GetCurveCount())
+                         - Shots[CurrentShot].Path.GetPoint(SplineProgress);
+
+            if ((float)Shots[CurrentShot].Path.GetCurveCount() - SplineProgress < 0.05f)
+            {
+                CurrentFade = FadeState.FadeOut;
+                FadeStartTime = Time.time;
+            }
+        }
+        else if (Time.time >= FadeStartTime + FadeLength)
+        {
+            if (CurrentFade == FadeState.FadeIn)
+            {
+                FadeStartTime = -1;
+                CurrentFade = FadeState.None;
+            } else
+            {
+                CurrentShot++;
+                CurrentShot %= Shots.Length;
+                SplineProgress = 0;
+                FadeStartTime = Time.time;
+
+                CurrentFade = FadeState.FadeIn;
+            }
+        }
+    }
 
 	private void ApplyShot() {
 		Camera.position = Shots[CurrentShot].Path.GetPoint(SplineProgress);
@@ -38,4 +85,9 @@ public class SplineCameraFollower : MonoBehaviour {
 		public Spline Path;
 		public float Velocity;
 	}
+
+    private enum FadeState
+    {
+       FadeOut, FadeIn, None
+    }
 }
